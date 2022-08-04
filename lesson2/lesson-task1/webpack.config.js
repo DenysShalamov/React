66dad500e7001e12bad4220a0e7619c1d8c82771
Denys Shalamov <shalamov.denys@gmail.com>
2022-08-04 22:10:17 +0300

    L2-LT2

--- lesson2/lesson-task1/webpack.config.js
+++ lesson2/lesson-task1/webpack.config.js
@@ -1,25 +1,25 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const webpack = require('webpack');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+// const webpack = require('webpack');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /.js$/,
-        use: ['babel-loader'],
-      },
-    ],
-  },
-  plugins: [
-    new webpack.ProgressPlugin(),
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-  ],
-};
+// module.exports = {
+//   entry: './src/index.js',
+//   output: {
+//     filename: 'bundle.js',
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /.js$/,
+//         use: ['babel-loader'],
+//       },
+//     ],
+//   },
+//   plugins: [
+//     new webpack.ProgressPlugin(),
+//     new CleanWebpackPlugin(),
+//     new HtmlWebpackPlugin({
+//       template: './src/index.html',
+//     }),
+//   ],
+// };
