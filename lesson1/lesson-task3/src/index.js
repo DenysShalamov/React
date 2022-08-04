7ecdfc1f23e9da3bb7fe113d8fddf9a5f2be001a
Denys Shalamov <shalamov.denys@gmail.com>
2022-08-04 17:14:32 +0300

    L1-LT4

--- lesson1/lesson-task3/src/index.js
+++ lesson1/lesson-task3/src/index.js
@@ -1,45 +1,46 @@
-// eslint-disable-next-line import/no-unresolved
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles.css';
-const rootElement = document.querySelector('#root');
+// // eslint-disable-next-line import/no-unresolved
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './styles.css';
 
-// const renderGreeting = elem => {
-//   const containerElem = document.createElement('div');
-//   containerElem.classList.add('greeting');
+// const rootElement = document.querySelector('#root');
 
-//   const titleElem = document.createElement('h1');
-//   titleElem.classList.add('greeting__title');
-//   titleElem.textContent = 'Hello, world!';
+// // const renderGreeting = elem => {
+// //   const containerElem = document.createElement('div');
+// //   containerElem.classList.add('greeting');
 
-//   const textElem = document.createElement('p');
-//   textElem.classList.add('greeting__text');
-//   textElem.textContent = 'I am learning React';
+// //   const titleElem = document.createElement('h1');
+// //   titleElem.classList.add('greeting__title');
+// //   titleElem.textContent = 'Hello, world!';
 
-//   containerElem.append(titleElem, textElem);
+// //   const textElem = document.createElement('p');
+// //   textElem.classList.add('greeting__text');
+// //   textElem.textContent = 'I am learning React';
 
-//   elem.append(containerElem);
-// };
+// //   containerElem.append(titleElem, textElem);
 
-// renderGreeting(rootElement);
+// //   elem.append(containerElem);
+// // };
 
-// const element = React.createElement(
-//   'div',
-//   { className: 'greeting' },
-//   React.createElement('div', { className: 'greeting__title' }, 'Hello, world!'),
-//   React.createElement(
-//     'div',
-//     { className: 'greeting__text' },
-//     "I'm learning React",
-//   ),
-// );
+// // renderGreeting(rootElement);
 
-// ReactDOM.render(element, rootElement);
-const element = (
-  <div className="greeting">
-    <div className="greeting__title">Hello, world!</div>
-    <div className="greeting__text">I'm learning React</div>
-  </div>
-);
+// // const element = React.createElement(
+// //   'div',
+// //   { className: 'greeting' },
+// //   React.createElement('div', { className: 'greeting__title' }, 'Hello, world!'),
+// //   React.createElement(
+// //     'div',
+// //     { className: 'greeting__text' },
+// //     "I'm learning React",
+// //   ),
+// // );
 
-ReactDOM.render(element, rootElement);
+// // ReactDOM.render(element, rootElement);
+// // const element = (
+// //   <div className="greeting">
+// //     <div className="greeting__title">Hello, world!</div>
+// //     <div className="greeting__text">I'm learning React</div>
+// //   </div>
+// // );
+
+// // ReactDOM.render(element, rootElement);
