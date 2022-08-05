9f92ee5fb6a8c61c5c018901060390e3e4615cb3
Denys Shalamov <shalamov.denys@gmail.com>
2022-08-05 15:28:15 +0300

    changed

--- lesson3/home-task2/src/Profile.jsx
+++ lesson3/home-task2/src/Profile.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import moment from 'moment';
+import './profile.scss';
 
 function Profile(userData) {
   const birthDate = moment(userData.user.birthDate).format('DD MMM YY');
   return (
-    <>
-      <div className="profile__name">{userData.user.firstName} {userData.user.lastName}</div>
-      <div className="profile__birth">Was born {birthDate} in {userData.user.birthPlace}</div>
-    </>
+    <div className="profile">
+      <h2 className="profile__name">
+        {userData.user.firstName} {userData.user.lastName}
+      </h2>
+      <div className="profile__birth">
+        Was born {birthDate} in {userData.user.birthPlace}
+      </div>
+    </div>
     
   )
 };
