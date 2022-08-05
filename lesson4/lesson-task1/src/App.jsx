12999d30fce805773fe33a34d4068043ece70cd3
Denys Shalamov <shalamov.denys@gmail.com>
2022-08-05 20:29:16 +0300

    L4-HT1

--- lesson4/lesson-task1/src/App.jsx
+++ lesson4/lesson-task1/src/App.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import Counter from './Counter';
+// import React from 'react';
+// import Counter from './Counter';
 
-const App = () => {
-  return (
-    <>
-      <Counter start={0} interval={3600000}/>
-      <Counter start={0} interval={60000}/>
-      <Counter start={0} interval={1000}/>
-    </>
-  );
-};
+// const App = () => {
+//   return (
+//     <>
+//       <Counter start={0} interval={3600000}/>
+//       <Counter start={0} interval={60000}/>
+//       <Counter start={0} interval={1000}/>
+//     </>
+//   );
+// };
 
-export default App;
+// export default App;
--- lesson4/lesson-task1/src/Counter.jsx
+++ lesson4/lesson-task1/src/Counter.jsx
@@ -1,35 +1,34 @@
-import React, { Component } from 'react';
-import './counter.scss';
+// import React, { Component } from 'react';
+// import './counter.scss';
 
-// const Counter = () => {
-//   return (
-//     <div className="counter">17</div>
-//   )
+// // const Counter = () => {
+// //   return (
+// //     <div className="counter">17</div>
+// //   )
+// // }
+// let counter = 0;
+
+// setInterval(() => {
+//   counter += 1;
+// }, 1000);
+
+// class Counter extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       counter: props.start,
+//     }
+
+//     setInterval(() => {
+//       this.setState({
+//         counter: this.state.counter + 1,
+//       });
+//       counter += 1;
+//     }, props.interval);
+//   }
+//   render() {
+//     return <div className="counter">{this.state.counter}</div>;
+//   }
 // }
-let counter = 0;
 
-setInterval(() => {
-  counter += 1;
-}, 1000);
-
-class Counter extends Component {
-  
-  constructor(props) {
-    super(props);
-    this.state = {
-      counter: props.start,
-    }
-
-    setInterval(() => {
-      this.setState({
-        counter: this.state.counter + 1,
-      });
-      counter += 1;
-    }, props.interval);
-  }
-  render() {
-    return <div className="counter">{this.state.counter}</div>;
-  }
-}
-
-export default Counter;
+// export default Counter;
