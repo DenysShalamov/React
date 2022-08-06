3f45330f898b217e343d0b9b41e7b10712485800
Denys Shalamov <shalamov.denys@gmail.com>
2022-08-06 11:50:27 +0300

    L5-LT2

---
+++ lesson5/lesson-task2/src/GoodButton.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './goodButton.scss';
+
+class GoodButton extends React.Component {
+  handleClick(event) {
+    console.log(event);
+    alert(event.target.textContent);
+  }
+  render() {
+    return <button className="fancy-button" onClick={this.handleClick}>Click me!</button>;
+  }
+}
+export default GoodButton;
