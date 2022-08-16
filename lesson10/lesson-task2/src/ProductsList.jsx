0f6ed9c186a08150ad5572b91a8c5ebb854c8869
Denys Shalamov <shalamov.denys@gmail.com>
2022-08-16 21:28:10 +0300

    changed

--- lesson10/lesson-task2/src/ProductsList.jsx
+++ lesson10/lesson-task2/src/ProductsList.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-class ProductList extends React.Component {
+class ProductsList extends React.Component {
   render() {
     const total = this.props.cartItems.reduce(
       (acc, item) => acc + item.price,
@@ -23,4 +23,4 @@ class ProductList extends React.Component {
   }
 }
 
-export default ProductList;
+export default ProductsList;
