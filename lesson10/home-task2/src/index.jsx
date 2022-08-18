f3d321898eda9e67a4e1c9d2c720fe13bbdd826b
Denys Shalamov <shalamov.denys@gmail.com>
2022-08-18 13:27:48 +0300

    L10-HT2

---
+++ lesson10/home-task2/src/index.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.scss';
+import App from './App';
+
+const rootElem = document.querySelector('#root');
+const root = createRoot(rootElem);
+
+const PRODUCTS = [
+  {
+    category: 'Sporting Goods',
+    price: '$49.99',
+    stocked: true,
+    name: 'Football',
+  },
+  {
+    category: 'Sporting Goods',
+    price: '$9.99',
+    stocked: true,
+    name: 'Baseball',
+  },
+  {
+    category: 'Sporting Goods',
+    price: '$29.99',
+    stocked: false,
+    name: 'Basketball',
+  },
+  {
+    category: 'Electronics',
+    price: '$99.99',
+    stocked: true,
+    name: 'iPod Touch',
+  },
+  {
+    category: 'Electronics',
+    price: '$399.99',
+    stocked: false,
+    name: 'iPhone 5',
+  },
+  { category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7' },
+];
+
+root.render(<App products={PRODUCTS} />);
